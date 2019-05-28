00365c72f16b9fb9580122810e60f95376817f70
majorkira <majorkira1@hotmail.com>
2019-05-28 10:42:04 -0400

    initial commit

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = () => {
+	return (
+	<div>
+	  <p className='f3'>
+	  	{'This will detect faces in your photos. Enter a URL to a photo to detect the face.'}
+	  </p>
+	  <div className='center'> 
+	  	<div className='form center pa4 br5 shadow-5'>
+	    	<input className='f4 pa2 w-70 center' type="text" />
+	    	<button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-blue'>Detect</button>
+	    </div>
+	  </div>
+	</div>
+	);
+}
+
+export default ImageLinkForm;
